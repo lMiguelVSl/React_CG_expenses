431339793fd9ee6a117dba635c01c342f5cc58e4
Miguel Vargas <miguel.vargas.work@outlook.com>
2023-06-26 23:13:44 -0500

    another props approach

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -4,10 +4,10 @@ function ExpenseItem(props) {
 
     return (
         <div className="expense-item">
-            <div>{props.Date.toISOString()}</div>
+            <div>{props.expense.Date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{props.Name}</h2>
-                <div className='expense-item__price'>{props.Price}</div>
+                <h2>{props.expense.Name}</h2>
+                <div className='expense-item__price'>{props.expense.Price}</div>
             </div>
         </div>
     )
